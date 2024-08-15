c2df18d5ce51b552cc6f192b889987ab9de172b0
Jigar <95428432+jigarsiddhpura@users.noreply.github.com>
2024-08-15 19:32:46 +0530

    style: add search bar in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,48 +1,55 @@
-import { FaShoppingCart } from "react-icons/fa";
-import { useSelector } from "react-redux";
+import React from 'react';
+import { FaShoppingCart, FaUser } from "react-icons/fa";
+import { FiSearch } from "react-icons/fi";
+import { AiOutlineMobile } from "react-icons/ai";
 import { NavLink } from "react-router-dom";
-
-
+import { useSelector } from "react-redux";
 
 const Navbar = () => {
-const {cart}= useSelector((state)=>state)
-
-  return(
-    <div>
-      <nav className="flex flex-row justify-between items-center h-20 max-w-6xl mx-auto">
-          <NavLink to='/'>
-            <div className="flex flex-row justify-between items-center gap-x-6 text-slate-100">
-              <div className="ml-6">
-                  <img src="https://upload.wikimedia.org/wikipedia/en/f/f6/Kendrick_Lamar_-_To_Pimp_a_Butterfly.png" className="h-14" alt="logo"/>  
-              </div>
-              <div>
-                  <h1 >To Pimp A Butterfly</h1>
-              </div>
-             
-            </div>
+  const {cart}= useSelector((state)=>state)
+  return (
+    <div className="border-b-2 bg-slate-100 border-slate-400">
+      <nav className="max-w-7xl mx-auto px-4 ">
+        <div className="flex justify-between items-center py-2">
+          <NavLink to='/' className="text-purple-700 text-3xl font-bold">
+            Auralis
           </NavLink>
+          <div className="flex-1 max-w-2xl mx-8">
+            <div className="relative border border-slate-500">
+              <input
+                type="text"
+                placeholder="Try Saree, Kurti or Search by Product Code"
+                className="w-full py-2 pl-10 pr-4 text-gray-700 bg-gray-100 rounded-sm focus:outline-none focus:bg-white focus:border focus:border-black-500 focus:bg-purple-700"
+              />
+              <FiSearch className="absolute left-3 top-3 text-gray-400" />
+            </div>
+          </div>
+          <div className="flex items-center space-x-12 text-sm">
 
-          <div className="flex flex-row items-center gap-x-6 mr-6 text-slate-100">
-            <NavLink to='/'>
-             <p>Home</p>
+            {/* <NavLink to="/profile" className="flex flex-col items-center">
+              <FaUser className=""/>
+              Profile
+            </NavLink> */}
+            <NavLink to="/" className="flex flex-col items-center text-lg hover:bg-purple-700 font-medium">
+              {/* <FaUser className=""/> */}
+              Home
             </NavLink>
-            
             <NavLink to='/cart'>
               <div className="relative">
-                <FaShoppingCart/>
+                <FaShoppingCart className="size-5"/>
                 {
                   cart.length > 0 &&
-                  <span className="absolute -top-1 -right-2 bg-green-600 rounded-full text-sm w-5 h-5 grid justify-items-center animate-bounce text-white">{cart.length}</span>
+                  <span className="absolute -top-1 -right-2 bg-purple-700 rounded-full text-sm w-5 h-5 grid justify-items-center animate-bounce text-white">{cart.length}</span>
                 }
                 
               </div>
             </NavLink>
-            
           </div>
+        </div>
+        
       </nav>
-
     </div>
-  )
+  );
 };
 
 export default Navbar;
