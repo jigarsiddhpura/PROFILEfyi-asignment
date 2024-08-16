d9eed5d79196c96d7c55ec099a96521b3e9cc6d9
Jigar <95428432+jigarsiddhpura@users.noreply.github.com>
2024-08-16 22:42:45 +0530

    style: spinner to PacmanLoader

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
-import "./Spinner.css"
+import React from 'react';
+import { PacmanLoader } from 'react-spinners';
 
-export const Spinner = () => {
+const Spinner = () => {
   return (
-    <div className='spinner'>Spinner</div>
-  )
-}
+    <div className="fixed inset-0 flex justify-center items-center bg-white/80 z-[9999]">
+      <PacmanLoader color="#9333ea" size={25} />
+    </div>
+  );
+};
+
+export default Spinner;
