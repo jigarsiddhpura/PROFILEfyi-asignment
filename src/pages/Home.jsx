bd9d3eed2c0fbc6992e036facd807e8873d2a0f7
Jigar <95428432+jigarsiddhpura@users.noreply.github.com>
2024-08-18 19:18:23 +0530

    fix: ensure products are visible on mobile

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,32 @@
 import { useEffect, useState } from "react";
 import Product from "../components/Product";
 import Spinner from "../components/Spinner";
-import { useProductData } from "../hooks/useProductData";
 import { useSearch } from "../hooks/useSearch";
 import Navbar from "../components/Navbar";
 
 const Home = () => {
+  const API_URL = "https://fakestoreapi.com/products";
   const [isMobile, setIsMobile] = useState(false);
-
-  const { loading, posts } = useProductData();
+  const [loading, setLoading] = useState(false);
+  const [posts, setPosts] = useState([]);
   const { searchTerms, filteredItems, handleSearch } = useSearch(posts);
+
+  async function fetchProductData() {
+    setLoading(true);
+    try {
+      const res = await fetch(API_URL);
+      const data = await res.json();
+      setPosts(data);
+    } catch (error) {
+      console.log("Error fetching data:", error);
+      setPosts([]);
+    }
+    setLoading(false);
+  }
 
   // TODO: CHECK DEVICE - MOBILE/DESKTOP
   useEffect(() => {
+    fetchProductData();
     const checkMobile = () => {
       setIsMobile(window.innerWidth <= 768);
     };
